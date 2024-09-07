b8f592f352f203055cf7d1c53ea118159888007b
Víctor Santiago <victorsantiago.matesub@gmail.com>
2024-09-07 09:21:18 +0200

    fix: configure base with missing slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "vik.sa.photography",
+  base: "/vik.sa.photography/",
 });
